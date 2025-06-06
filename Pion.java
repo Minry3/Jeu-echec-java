ac4a53ae451c4fc976f24d2af590e3ebc1c736d1
Clarence E. <clarenceedohdagnon@gmail.com>
2025-06-06 23:07:42 +0200

    Modification de la classe Pion

--- Pion.java
+++ Pion.java
@@ -7,7 +7,22 @@ public class Pion extends Piece
 
     public boolean deplacement(Case destination)
     {
-        return false;
+        int colonneDepart = Echiquier.indiceEtiquette(this.getCase().getEtiquette());
+        int colonneArrivee = Echiquier.indiceEtiquette(destination.getEtiquette());
+        int ligneDepart = Echiquier.indiceNumero(this.getCase().getNumero());
+        int ligneArrivee = Echiquier.indiceNumero(destination.getNumero());
+        boolean deplacementSansCapture = false;
+        boolean deplacementCapture = false;
+        if(this.getCouleur().equals("blanc")){
+            deplacementSansCapture = (colonneDepart == colonneArrivee) && ((ligneArrivee - ligneDepart ) == 1);
+            deplacementCapture = ((ligneArrivee - ligneDepart) == 1) && (Math.abs(colonneDepart - colonneArrivee) == 1);
+        }
+        else{
+            deplacementSansCapture = (colonneDepart == colonneArrivee) && ((ligneDepart - ligneArrivee ) == 1);
+            deplacementCapture = ((ligneDepart - ligneArrivee) == 1) && (Math.abs(colonneDepart - colonneArrivee) == 1);
+        }
+        
+        return deplacementCapture || deplacementSansCapture;
     }
 
     public String toString()
