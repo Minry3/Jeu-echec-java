03c6ce180bdb3e963aea1cc91c2cc18d32243d7e
Clarence E. <clarenceedohdagnon@gmail.com>
2025-06-06 22:56:47 +0200

    Ajout de la classe Joueur

---
+++ Joueur.java
@@ -0,0 +1,28 @@
+
+public class Joueur {
+    private String nom;
+    private String couleur;
+
+
+    public Joueur(String unNom, String uneCouleur){
+        this.nom = unNom;
+        this.couleur = uneCouleur;
+    }
+
+    public String getNom(){
+        return this.nom;
+    }
+
+    public String getCouleur(){
+        return this.couleur;
+    }
+    
+    public void setNom(String unNom){
+        this.nom = unNom;
+    }
+
+    public void setCouleur(String uneCouleur){
+        this.couleur = uneCouleur;
+    }
+
+}
