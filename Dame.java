90cb8555b22ea25448ea1d708ecb6c5b5f362133
Clarence E. <clarenceedohdagnon@gmail.com>
2025-06-06 23:06:46 +0200

    Modification de la classe Dame

--- Dame.java
+++ Dame.java
@@ -1,17 +1,26 @@
-public class Dame extends Piece
-{
-    public Dame(String couleur)
-    {
+public class Dame extends Piece {
+
+    public Dame(String couleur){
+
         super(couleur);
     }
 
-    public boolean deplacement(Case destination)
-    {
-        return false;
-    }
+    public boolean deplacement(Case destination){
 
-    public String toString()
-    {
+        int colonneDepart = Echiquier.indiceEtiquette(this.getCase().getEtiquette());
+        int colonneArrivee = Echiquier.indiceEtiquette(destination.getEtiquette());
+        int ligneDepart = Echiquier.indiceNumero(this.getCase().getNumero());
+        int ligneArrivee = Echiquier.indiceNumero(destination.getNumero());
+
+        boolean deplacementHorizontalOuVertical =  colonneDepart == colonneArrivee || ligneDepart == ligneArrivee;
+        boolean deplacementDiagonale = Math.abs(colonneArrivee - colonneDepart) == Math.abs(ligneArrivee - ligneDepart);
+
+        return deplacementHorizontalOuVertical || deplacementDiagonale;
+}
+
+
+    public String toString(){
+
         if(this.getCouleur().equals("blanc"))
             return " ♛ " ;
         return " ♕ ";
